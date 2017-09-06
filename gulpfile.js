c3947e7fc52e02bba325f95dcfcb082c2664f422
brownae <aebrown9@gmail.com>
2017-09-06 09:47:52 -0700

    Restructures files and implements Gulp
    I added node_modules to .gitignore

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const del = require('del');
+var browserSync = require('browser-sync').create();
+
+/*
+--Top level functions--
+    gulp.task -- Define tasks
+    gulp.src -- Point to files to use
+    gulp.dest -- Points to the folder to output
+    gulp.watch -- watch files
+
+*/
+
+//Copy all files with .php extension & put in correct place
+gulp.task('copyPhp', function(){
+    gulp.src('src/*.php')
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('src/includes/**/*')
+        .pipe(gulp.dest('dist/includes'));
+});
+
+//Optimizes images
+gulp.task('imageMin', function(){
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+    //Just move svg's over with images
+    gulp.src('src/images/icons/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images/icons'));
+});
+
+//minify js
+gulp.task('uglify', function(){
+    gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+//concat css
+gulp.task('copyCss', function(){
+    gulp.src('src/css/*.css')
+        .pipe(gulp.dest('dist/css'));
+});
+
+//Delete everything in the dist file so it can be re-written
+gulp.task('clean', function () {
+  return del([
+    'dist/*/**'
+  ]);
+});
+
+//runSequnce I had to install to keep scripts from running simultaniously
+gulp.task('default', function(callback) {
+    runSequence('clean',['copyCss','copyPhp', 'imageMin','uglify'],callback);
+});
+
+gulp.task('sync', function() {
+    gulp.watch('src/*.php',['copyPhp']);
+    gulp.watch('src/includes/*.php',['copyPhp']);
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/css/*.css', ['copyCss']);
+    browserSync.init({
+        proxy: "http://localhost:8888/1portfolio/dist/",
+        files: "*.css,*.php,css/*css"
+    });
+});
+
+//Note Uglify will not happen when watching
+gulp.task('watch', function(){
+    gulp.watch('src/*.php',['copyPhp']);
+    gulp.watch('src/includes/*.php',['copyPhp']);
+    gulp.watch('src/images/*', ['imageMin']);
+    gulp.watch('src/css/*.css', ['copyCss']);
+
+});
---
+++ src/index.php
@@ -0,0 +1,152 @@
+<?php include("includes/top.php");?>
+
+<div id="home-page">
+    <section class="module parallax parallax-1">
+        <div class="container">
+            <img src="images/BrownAE-logo.svg" alt="Brownie Web Design Logo"/>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container">
+            <h3>Hello, my name is</h3>
+            <h2>Aaron E. Brown</h2>
+            <p class="center">(Aka Brown or "Brownie")</p>
+        </div>
+        <div class="intro-graphics-wrapper">
+            <div class="intro-graphic">
+                <img src="images/developer.svg" alt="Front-end developer icon" title="Front End Developer">
+                <h3>Front-End Developer</h3>
+            </div>
+            <div class="intro-graphic">
+                <img src="images/retroatom.svg" alt="Designer" title="Designer">
+                <h3>Designer</h3>
+            </div>
+            <div class="intro-graphic">
+                <img src="images/Thumb-up.svg" alt="People Person" title="People Person">
+                <h3>People Person</h3>
+            </div>
+        </div>
+    </section>
+    <section class="content">
+        <div class="seo">
+            <p>As a Seattle based front-end web developer and designer, I enjoy building simple rich UX/UI that are beautiful, easy to learn/understand and use. I work well with others and I am passionate about clear communication, solid solutions, understanding client &amp; user
+            needs, then designing and building a platform that intuitively meets the needs of all.</p>
+        </div>
+        <a name="portfolio"></a><!-- Put here to make the location more accurate -->
+</section>
+
+<section class="content">
+    <h2 class="center">Portfolio</h2>
+    <div class="wrapperDemos">
+
+        <div class="demo-graphics">
+            <img src="images/Canon-Bar-Demo2.png" alt="Canon cocktail bar website" title="Canon cocktail bar website">
+            <h4>Canon Whiskey &amp; Bitters Emporium</h4>
+            <p>
+                <a href='http://canon.brownae.com/' target="_blank" ><i>Website</i></a> |
+                <a href="#"><i>Summary</i></a>
+                <!-- website on MY server... http://canon.brownae.com/ -->
+            </p>
+        <p>Website redesign. UX/UI, backend created using Scaphold.io and graphQL.</P>
+        </div>
+
+        <div class="demo-graphics">
+            <img src="images/neat-shaving-co-Demo.png" alt="Neat Shaving Co. website" title="Neat Shaving Co. website">
+            <h4>Neat Shaving Co.</h4>
+            <p>
+                <a href='http://www.scc.brownae.com/seattlecentral/web202/protosite/index.php' target="_blank"><i>Website</i></a> | <a href="http://www.blog.brownae.com/neat-shaving-co/"><i>Summary</i></a>
+
+            </p>
+            <p>Brief case study. Designed Logo, UX/UI, Checkout procedure flow. Currently in a prototype phase.</P>
+        </div>
+
+        <div class="demo-graphics">
+            <img src="images/Nafanua-Demo.png" alt="Nafanua book website" title="Nafanua book website">
+            <h4>Nafanua - Book by Paul Alan Cox</h4>
+            <p>
+                <a href='http://www.nafanua.org/' target="_blank"><i>Website</i></a> | <a href="https://docs.google.com/document/d/144Pc_zMFsLr3YJFNo3U_W26AsId44km8lwG8NwciHkk/edit?usp=sharing"><i>Summary</i></a>
+                <!-- website on MY server... http://www.scc.brownae.com/seattlecentral/cis197/protosite2/legend.php -->
+            </p>
+            <p>Website redesign for launch of digital republish of book. UX/UI, brochure site with links to purchase book.</P>
+        </div>
+
+        <div class="demo-graphics">
+            <img src="images/Lift-Every-Voice-Foundation-Demo.png" alt="The Lift Every Voice Foundation" title="The Lift Every Voice Foundation">
+            <h4>Lift Every Voice Foundation</h4>
+            <p>
+                <a href='http://www.thelifteveryvoicefoundation.org/' target="_blank"><i>Website</i></a> | <a href="#"><i>Summary</i></a>
+            </p>
+            <p>Custom built wordpress site. Designed UX/UI, coded design with HTML, PHP &amp; CSS. Uploaded to clients url. entire site updatable by client.</P>
+        </div>
+
+        <div class="demo-graphics">
+            <img src="images/Scoot-aboot-Demo.png" alt="Scoot Aboot" title="Banff Vespa tour">
+            <h4>Scoot Aboot</h4>
+            <p>
+                <a href='http://scootaboot.com/' target="_blank"><i>Website</i></a> | <a href="#"><i>Summary</i></a>
+            </p>
+            <p>Website built for a road trip. Minimal and lightweight, brochure site. </P>
+        </div>
+
+        <div class="demo-graphics">
+            <img src="images/Laissez-faire-studios-Demo.png" alt="Laiessez Faire Studios" title="Laiessez Faire Studios">
+            <h4>Laiessez Faire Studios</h4>
+            <p>
+                <a href='http://www.scc.brownae.com/seattlecentral/web120/fp/index.php' target="_blank"><i>Website</i></a> | <a href="#"><i>Summary</i></a>
+            </p>
+            <p>Website for Laissez faire studio was built entirely for client then decided not to persue studio. </P>
+        </div>
+    </div>
+</section>
+
+<section class="content">
+    <h2 class="center">Languages &amp; Tools</h2>
+    <h3>Current + <span>In Process</span></h3>
+    <div class="dev-icons wrapperIcons">
+        <img src="images/icons/html5.svg" alt="HTML5 logo" title="HTML5">
+        <img src="images/icons/css3.svg" alt="CSS3 logo" title="CSS3">
+        <img src="images/icons/javascript.svg" alt="JavaScript logo" title="JavaScript">
+        <img src="images/icons/jquery_logo.svg" alt="jQuery logo" title="jQuery">
+        <img src="images/icons/nodejs_small.svg" alt="Node.js logo" title="Node.js">
+        <img src="images/icons/angular_simple.svg" alt="AngularJS logo" title="AngularJS">
+        <img src="images/icons/php.svg" alt="PHP logo" title="PHP">
+        <img src="images/icons/mysql.svg" alt="mySQL logo" title="mySQL">
+        <img src="images/icons/graphQL.svg" alt="GraphQL logo" title="GraphQL">
+        <img src="images/icons/github_badge.svg" alt="GitHub logo" title="GitHub">
+        <img src="images/icons/grunt.svg" alt="Grunt logo" title="Grunt">
+        <img src="images/icons/gulp.svg" alt="Gulp logo" title="Gulp">
+        <img src="images/icons/sass.svg" alt="Sass logo" title="Sass">
+        <img src="images/icons/illustrator.svg" alt="Adobe Illustrator logo" title="Adobe Illustrator">
+        <img src="images/icons/photoshop.svg" alt="Adobe Photoshop logo" title="Adobe Photoshop">
+        <img src="images/icons/Sketchapp-logo.svg" alt="Sketch App logo" title="Sketchapp">
+        <img src="images/icons/atom.svg" alt="Atom logo" title="Atom.io">
+    </div>
+
+    <a name="contact"></a> <!-- Put here to make the location more accurate -->
+</section>
+
+<section class="module content">
+  <div class="container">
+    <h2>Contact</h2>
+    <div id="contact-form">
+      <?php
+        /*
+         * Below are 2 different forms to be re-used
+         *
+         * Only use one at a time, comment out the other 1!
+         *
+         */
+        include "includes/simple.php"; #demonstrates a simple contact form
+        //include "includes/multiple.php";#demonstrates multiple form elements
+
+
+	?>
+	 <p class="clear-recaptcha"></p>
+        </div>
+  </div>
+</section>
+
+</div>
+
+<?php include ("includes/bottom.php");?>
